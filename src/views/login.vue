4eb748909c4f1c79c1fe8f0256808918b6bdff69
Zelalem <zola@programmer.net>
2018-12-06 16:52:06 +0300

    proper login page style

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,27 +1,32 @@
 <template>
+<v-container fill-height>
 
-<v-container>
+    <v-layout column wrap align-center justify-center>
 
-    <v-card>
+        <img src="@/assets/logo.png" width="300" class="ma-5">
+ 
+        <v-card width="400">
 
-        <v-card-title primary-title>
-            <h1>Login</h1>
-        </v-card-title>
-        
-        <v-card-text>
-            <v-form id="login-form" v-model="valid">
-                <v-text-field v-model="user.email" :error="errors.email" label="Email..." required></v-text-field>
-                <v-text-field v-model="user.password" :error="errors.password" label="Password..." required></v-text-field>
-            </v-form>
-        </v-card-text>
+            <v-card-title primary-title>
+                <h1>Login</h1>
+            </v-card-title>
 
-        <v-card-actions>
-            <v-btn color="primary" block :loading="loading" :disabled="disabled">Login</v-btn>
-        </v-card-actions>
-    </v-card>
+            <v-card-text>
+                <v-form id="login-form" v-model="valid">
+                    <v-text-field v-model="user.email" :error="errors.email" label="Email..." required></v-text-field>
+                    <v-text-field v-model="user.password" :error="errors.password" label="Password..." required></v-text-field>
+                </v-form>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-btn color="primary" block :loading="loading" :disabled="disabled">Login</v-btn>
+            </v-card-actions>
+
+        </v-card>
+
+    </v-layout>
 
 </v-container>
-
 </template>
 
 <script>
