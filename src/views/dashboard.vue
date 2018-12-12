435294adc2ad6e77dc6ca2c386a9eb46661c97c3
Zelalem <zola@programmer.net>
2018-12-12 09:12:49 +0300

    stats on dashboard

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -5,13 +5,13 @@
         <v-select v-model="documentId" :items="documents" label="Document" item-text="title" item-value="id"></v-select>
     </v-card-title>
 
-    <v-card-text v-if="stats">
+    <v-card-text>
 
-        <h2>Total Number Of Annotations</h2>
+        <h2 class="center">TOTAL NUMBER OF ANNOTATIONS</h2>
 
-        <h2>{{ stats.totalAnnotations }}</h2>
+        <h2 class="center">{{ stats.totalAnnotations }}</h2>
 
-        <h2>Reviews And Comments Per Section</h2>
+        <h2 class="center">REVIEWS AND COMMENTS PER SECTION</h2>
 
         <v-layout row wrap>
 
@@ -20,13 +20,13 @@
                 <v-card class="ma-2">
                     <v-card-title primary>
 
-                        <h4>{{ title }}</h4>
+                        <h4 class="center">{{ title }}</h4>
 
                     </v-card-title>
 
                     <v-card-text>
 
-                        <v-layout row wrap>
+                        <v-layout class="center" row wrap>
 
                             <v-flex xs6>
                                 {{ stat.reviews }} Reviews
@@ -46,16 +46,35 @@
 
         </v-layout>
 
+        <v-layout justify-center class="pa-4">
+            <v-card class="pa-4">
+                <v-card-title>
+                    <div style="text-align: center; width: 100%;">
+                        <img src="@/assets/logo.png" width="100">
+                    </div>
+                </v-card-title>
+                <v-card-text>
+                    <canvas width="600" height="300" ref="canvas"></canvas>
+                </v-card-text>
+                <v-card-title>
+                    <h4 style="text-align: center; width: 100%;">Engagement report</h4>
+                </v-card-title>
+            </v-card>
+        </v-layout>
+
     </v-card-text>
+
 </v-card>
 </template>
 
 <script>
+import Chart from "chart.js";
+
 export default {
     name: "dashboard",
     data() {
         return {
-            stats: null,
+            stats: {},
             documentId: null,
             documents: []
         }
@@ -70,14 +89,69 @@ export default {
 
     },
 
-    watch: {
-        async documentId() {
+    methods: {
+        async load() {
 
             this.stats = await this.$store.dispatch("getObjects", {
                 path: `documents/stats?documentId=${this.documentId}`
             });
 
+            console.log(this.$refs.canvas);
+
+            new Chart(this.$refs.canvas, {
+                type: "bar",
+                data: this.barData,
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: true
+                },
+                scales: {
+                    yAxis: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Users'
+                        }
+                    }]
+                }
+            });
+        }
+    },
+
+    computed: {
+        barData() {
+
+            let data = {
+                labels: [],
+                datasets: [{
+                    label: "Engagement Report",
+                    backgroundColor: "#009688",
+                    data: []
+                }]
+            };
+
+            for (let t in this.stats.sections) {
+                let s = this.stats.sections[t];
+                data.labels.push(t);
+                data.datasets[0].data.push(s.totalEngagement);
+            }
+
+            return data;
+
+        }
+    },
+
+    watch: {
+        documentId() {
+            this.load();
         }
     }
 }
 </script>
+
+<style>
+.center {
+    text-align: center;
+    padding: 1rem;
+    width: 100%;
+}
+</style>
